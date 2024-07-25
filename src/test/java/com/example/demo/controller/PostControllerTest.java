638af1936eca4fe8ee326f1608db83b1fab0dee2
blackjune67 <buea486@gmail.com>
2024-07-25 13:57:01 +0900

    PostCreateController 테스트 코드 작성

---
+++ src/test/java/com/example/demo/controller/PostControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+public class PostControllerTest {
+
+}
---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,23 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`,`last_login_at`)
+values (1,
+        'buea486@gmail.com',
+        'june67',
+        'Seoul',
+        'aaaaaa-aa-aaaaa-aaaaa-aaaaaa',
+        'ACTIVE',
+        0);
+
+insert into `users` (`id`,
+                     `email`,
+                     `nickname`,
+                     `address`,
+                     `certification_code`,
+                     `status`,
+                     `last_login_at`)
+values (2,
+        'buea496@gmail.com',
+        'june68',
+        'Seoul',
+        'aaaaaa-aa-aaaaa-aaaaa-aaaaaa',
+        'PENDING',
+        0);
