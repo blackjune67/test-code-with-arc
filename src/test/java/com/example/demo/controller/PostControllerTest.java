c971c459630d0ca05940fd1b9a4bef9dcff90676
blackjune67 <buea486@gmail.com>
2024-07-25 14:37:57 +0900

    PostCreateController 테스트 코드 작성

--- src/test/java/com/example/demo/controller/PostControllerTest.java
+++ src/test/java/com/example/demo/controller/PostControllerTest.java
@@ -1,17 +1,81 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.PostUpdateDto;
+import com.example.demo.repository.PostRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 @SqlGroup({
+        @Sql(value = "/sql/post-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
         @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 })
 public class PostControllerTest {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    void postId로_단_건_게시물을_갖고_올_수_있다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/posts/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.content").value("testpost"))
+                .andExpect(jsonPath("$.writer.id").isNumber())
+                .andExpect(jsonPath("$.writer.email").value("buea486@gmail.com"));
+    }
+
+    @Test
+    void 사용자가_존재하지_않는_경우_게시물을_조희할_경우_에러가_난다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/posts/11234"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Posts에서 ID 11234를 찾을 수 없습니다."));
+    }
+
+    @Test
+    void 게시물을_수정할_수_있다() throws Exception {
+        PostUpdateDto postUpdateDto = PostUpdateDto.builder()
+                .content("helloworld!")
+                .build();
+        // given
+//        PostEntity result = postRepository.getReferenceById(1L);
+
+        // when
+        mockMvc.perform(put("/api/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postUpdateDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value("helloworld!"))
+                .andExpect(jsonPath("$.writer.id").isNumber())
+                .andExpect(jsonPath("$.writer.email").value("buea486@gmail.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("june67"));
+        // then
+    }
+
 
 }
---
+++ src/test/resources/sql/post-controller-test-data.sql
@@ -0,0 +1,3 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`) values (1, 'buea486@gmail.com', 'june67', 'Seoul', 'aaaaaa-aa-aaaaa-aaaaa-aaaaaa', 'ACTIVE', 0);
+
+insert into `posts` (`id`, `content`, `created_at`, `modified_at`, `user_id`) values (1, 'testpost', 2024072510133223, 0, 1);
